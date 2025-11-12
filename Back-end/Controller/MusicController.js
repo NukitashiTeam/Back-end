b3fcc5128f9443ada7a701ae71c95e513ee1bf8a
khanhpear2004 <khanh.le31072004@hcmut.edu.vn>
2025-11-12 23:00:00 +0700

    deploy thử lên gg cloud và thêm API phụ cho music

--- Back-end/Controller/MusicController.js
+++ Back-end/Controller/MusicController.js
@@ -6,9 +6,56 @@ const path = require('path');
 
 const Music = require('../Model/MusicSchema'); // import MusicSchema
 
+// @ts-ignore
+exports.checkMusicInItunes = async (req, res) => {
+  try{
+    const { keyword, numberOfsong } = req.query;
+    if (!keyword) return res.status(400).json({ message: 'Thiếu keyword' });
+
+    const url = `https://itunes.apple.com/search?term=${encodeURIComponent(keyword)}&entity=musicTrack&limit=${encodeURIComponent(numberOfsong)}`;
+    const response = await axios.get(url);
+    const results = response.data.results;
+
+    // @ts-ignore
+    const musics = results.map(track => ({
+      track_id: track.trackId.toString(),
+      title: track.trackName,
+      artist: track.artistName,
+      album: track.collectionName,
+      genre: track.primaryGenreName,
+      mp3_url: track.previewUrl,
+      image_url: track.artworkUrl100,
+      is_premium: track.trackPrice > 0, // ví dụ: >0 là premium
+      release_date: track.releaseDate,
+      mood: '',
+    }));
+
+    // Chỉ trả về preview, chưa lưu
+    res.status(200).json({ message: 'Dữ liệu preview', preview: musics });
+
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
+
 
 // @ts-ignore
-exports.addMusic = async (req, res) => {
+exports.addMusicsAfterReview = async (req, res) => {
+  try {
+    const musics = req.body;
+    const inserted = await Music.insertMany(musics);
+
+    res.status(200).json({ message: 'Đã lưu vào MongoDB', data: inserted });
+  } catch (err) {
+    res.status(500).json({ message: 'Lỗi khi lưu'});
+  }
+};
+
+
+// @ts-ignore
+exports.addMusics = async (req, res) => {
   try {
     const { keyword, numberOfsong } = req.body;
     if (!keyword) return res.status(400).json({ message: 'Thiếu từ khóa tìm kiếm (keyword)' });
@@ -18,7 +65,6 @@ exports.addMusic = async (req, res) => {
     const response = await axios.get(url);
     const results = response.data.results;
 
-    // Map dữ liệu sang schema của bạn
     // @ts-ignore
     const musics = results.map(track => ({
       track_id: track.trackId.toString(),
@@ -36,21 +82,9 @@ exports.addMusic = async (req, res) => {
     // Lưu vào MongoDB
     const inserted = await Music.insertMany(musics);
 
-    // Lưu vào file JSON
-    const filePath = path.join(__dirname, '../data/musicData.json');
-
-    let existingData = [];
-    if (fs.existsSync(filePath)) {
-        const raw = fs.readFileSync(filePath, 'utf8');
-        existingData = JSON.parse(raw);
-    }
-
-    // Gộp dữ liệu mới vào
-    const updatedData = [...existingData, ...inserted];
-    fs.writeFileSync(filePath, JSON.stringify(updatedData, null, 2), 'utf-8');
-
+  
     res.status(200).json({
-      message: 'Thêm nhạc thành công (đã lưu Mongo + JSON)',
+      message: 'Thêm nhạc thành công',
       data: inserted,
     });
 
@@ -60,6 +94,7 @@ exports.addMusic = async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 };
+
 
 // @ts-ignore
 exports.getAllMusic = async (req, res) => {
@@ -71,6 +106,7 @@ exports.getAllMusic = async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 };
+
 
 // @ts-ignore
 exports.getMusicById = async (req, res) => {
@@ -85,6 +121,36 @@ exports.getMusicById = async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 };
+
+
+// @ts-ignore
+exports.updateMusic = async (req, res) => {
+  try{
+    const updateData = req.body; 
+    const music = await Music.findByIdAndUpdate(req.param.id, updateData, { new: true } );
+
+    if (!updateData) {
+      return res.status(404).json({ message: 'Không tìm thấy bài nhạc này' });
+    }
+    res.status(200).json(updateData);
+
+  } catch(err){
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
+
+
+// // @ts-ignore
+// exports.updateMusicMood = async (req, res) => {
+//   try{
+
+//   }
+//   catch (err) {
+
+//   }
+// }
+
 
 // @ts-ignore
 exports.deleteMusic = async (req, res) => {
--- Back-end/Router/MusicRouter.js
+++ Back-end/Router/MusicRouter.js
@@ -6,14 +6,26 @@ const MusicController = require('../Controller/MusicController');
 
 // Gán controller vào router
 
-//Thêm 1 bài hát mới vào router - số lượng bài hát thêm vào có thể nhiều hơn 1
-router.post('/add-music', MusicController.addMusic);
+//Kiểm tra bài hát trên itunes
+router.get('/preview', MusicController.checkMusicInItunes);
 
 //Tìm kiếm bài hát
 router.get('/music-list', MusicController.getAllMusic);
 
 //Tìm kiếm theo ID bài hát
 router.get('/music-by-id/:id', MusicController.getMusicById);
+
+//Lấy các giá trị được fetch ra từ /Preview và insert vào db
+router.post('add-musics-after-review', MusicController.addMusicsAfterReview);
+
+//Thêm 1 bài hát mới vào router - số lượng bài hát thêm vào có thể nhiều hơn 1 - Khi check trên itunes sẽ thêm vào db ngay lập tức
+router.post('/add-musics', MusicController.addMusics);
+
+//update thông tin 1 bài hát qua id của nó
+router.put('music-all-data/:id', MusicController.updateMusic);
+
+//update thông tin mood của  1 bài hát qua id của nó
+// router.put('music-mood/:id', MusicController.updateMusicMood);
 
 //Xóa bài hát theo ID
 router.delete('/music-by-id/:id', MusicController.deleteMusic);
